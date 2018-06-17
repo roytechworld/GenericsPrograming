b67af61037b63b21930b18df57497dc43b71403b
roy <roy@192.168.2.4>
2018-06-17 11:34:28 +0530

    ss

---
+++ Springtest3/src/main/java/com/example/demo/Springtest3Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtest3Application.class, args);
+	}
+}
